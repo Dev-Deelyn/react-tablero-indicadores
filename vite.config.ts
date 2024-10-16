88f7ca3d0011e3a8d478ca7631f6f6fa857dd2d5
Hectorv72 <hectorvaldezfsa13@gmail.com>
2024-10-16 08:48:09 -0300

    test fix por --jsx

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    alias: {
-      "styled-components": 'src/styled-components',
-      config: 'src/config',
-      pages: 'src/pages',
-      containers: 'src/containers',
-      router: 'src/router',
-      components: 'src/components',
-      assets: 'src/assets',
-      helpers: 'src/helpers',
-      hooks: 'src/hooks',
-      contexts: 'src/contexts',
-      types: 'src/types',
-      utils: 'src/utils',
-    }
-  }
 })
