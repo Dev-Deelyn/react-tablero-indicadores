df16363e96be9e1b0f28eba60dbaba0fc9143d32
Dev-Deelyn <dev.deelyn@gmail.com>
2024-10-16 12:25:20 -0300

    Gráfico de líneas funcionando

--- src/utils/transformData.ts
+++ src/utils/transformData.ts
@@ -7,7 +7,7 @@ export const transformData = (data: Dataset[], dimension: string, metrics: strin
   const categories = data.map(item => item[dimension].toString());
   const series = metrics.map(metric => ({
     name: metric,
-    type: 'line' as const, // Asegurar que el tipo sea siempre 'line'
+    type: 'line' as const, // Asegurar que el tipo sea siempre 'line' 
     data: data.map(item => item[metric] as number),
   }));
   return { categories, series };
